dd68b4fcc6f4c41c71a0769af341b5e32d961e29
Egor Sergeenko <es.egor.sergeenko@gmail.com>
2023-03-30 15:43:00 +0300

    Fix images width

--- app.py
+++ app.py
@@ -211,8 +211,8 @@ def show_contacts(container):
     container.header('Contact us')
     columns = container.columns([3, 2, 2])
     text_column, image_columns = columns[0], columns[1:]
-    image_columns[0].image(load_image('itmo'), width=300)
-    image_columns[1].image(load_image('scamt'), width=300)
+    image_columns[0].image(load_image('itmo'), width=275)
+    image_columns[1].image(load_image('scamt'), width=275)
     text_column.text(
         """
         Anastasiia Orlova       orlova@scamt-itmo.ru
